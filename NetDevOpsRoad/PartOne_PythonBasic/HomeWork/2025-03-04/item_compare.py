15a325827cd84610caae955ed684824a752e79f6
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-04 09:58:28 +0800

    2025-3-4 homework alert print.

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-04/item_compare.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-04/item_compare.py
@@ -4,6 +4,6 @@ list2 = ['bbb', 333, 111, 3.14, (4,5)]
 
 for item in list1:
     if item in list2:
-        print(f"{item} only in list1 and list2")
+        print(f"{item} in list1 and list2")
     else:
-        print(f"{item} in list1")
+        print(f"{item} only in list1")
