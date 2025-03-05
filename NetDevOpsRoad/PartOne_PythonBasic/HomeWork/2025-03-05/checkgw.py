88a4999d8c111cde23108b26255f4a931ef06b8d
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-05 11:54:28 +0800

    2025-3-5 homework.

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-05/checkgw.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-05/checkgw.py
@@ -1,18 +1,21 @@
+import re
+import paramiko
 from ssh import qytang_ssh
 
-import os
-import re
-
-# route_n_result = os.popen("route -n").read()
-#
-# # print("\n")
-# # print('>ifconfig_result'+'-'*32)
-# # print(route_n_result)
-#
-# gateway = re.findall(r'(\d{1,3}(?:\.\d{1,3}){3})\s+\S+\s+UG', route_n_result)[0]
-#
-# print(f'网关为: {gateway}')
+def ssh_get_route(ip, username, password, port=22, cmd='route -n'):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=ip, port=port, username=username, password=password, timeout=5, compress=True)
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        x = stdout.read().decode('utf-8')
+        gateway = re.findall(r'(\d{1,3}(?:\.\d{1,3}){3})\s+\S+\s+UG', x)[0]
+        return gateway
+    except Exception as e:
+        print(f'Fail to get route gateway {ip}!{e}')
 
 if __name__ == '__main__':
     print(qytang_ssh('192.168.64.16', 'root', 'B1216.123'))
-    print(qytang_ssh('192.168.64.16', 'root', 'B1216.123', cmd='route -n'))
+    print(qytang_ssh('192.168.64.16', 'root', 'B1216.123', cmd='pwd'))
+    print('Gateway:')
+    print(ssh_get_route('192.168.64.16', 'root', 'B1216.123'))
