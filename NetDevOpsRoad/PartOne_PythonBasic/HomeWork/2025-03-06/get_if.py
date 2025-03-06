a360aaad13539444889434378c1aaaa7b518c4f0
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-06 14:49:38 +0800

    2025-3-6 homework.decrease multi-threads to 8 and add ping_result.type == 0 check.

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-06/get_if.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-06/get_if.py
@@ -1,3 +1,7 @@
+from re import match
+
+from setuptools.discovery import remove_stubs
+
 from ping import ping_subnet
 from ssh import qytang_ssh
 
@@ -7,16 +11,17 @@ import pprint
 def qytang_get_if(*ips, username='admin', password='Cisc0123'):
     device_if_dict = {}
     for ip in ips:
-        if ip != '192.168.64.1':
+        if ip == '192.168.64.1':
             continue
         interface_info = qytang_ssh(ip, username, password, cmd='show ip int brief')
-        # print(interface_info)
-        # print('='*32)
+        print(interface_info)
+        print('='*32)
 
         if_dict = {}
         for str in interface_info.split('\n'):
             re_result = re.match(r'(GigabitEthernet\d+)\s+(\d{1,3}(?:\.\d{1,3}){3})',str).groups()
-            if_dict[re_result[0]] = re_result[1]
+            if re_result:
+                if_dict[re_result[0]] = re_result[1]
 
         device_if_dict[ip] = if_dict
 
