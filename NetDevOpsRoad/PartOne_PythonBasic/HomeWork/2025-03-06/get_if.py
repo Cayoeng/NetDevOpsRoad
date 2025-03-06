7e38f6825012cdb5f0db3f251f85bf503a8210b3
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-06 13:56:13 +0800

    2025-3-6 homework.decrease multi-threads to 8 and add ping_result.type == 0 check.

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-06/get_if.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-06/get_if.py
@@ -1,14 +1,20 @@
 from ping import ping_subnet
 from ssh import qytang_ssh
 
-subnet = input("Please SUBNET you want to ping: ")
+import re
+import pprint
+
+def qytang_get_if(*ips, username='admin', password='Cisc0123'):
+    device_if_dict = {}
+    for ip in ips:
+        device_if_dict[ip] = qytang_ssh(ip, username, password, cmd='show ip int brief')
+    return device_if_dict
+
+
+subnet = input('Please SUBNET you want to ping: ')
 
 hosts = ping_subnet(subnet)
 
-print(f"There are {len(hosts)} available hosts in subnet {subnet}: ")
-
-print(hosts)
-
-# interface_list = {}
-# for host in hosts:
-#     qytang_ssh(host, 'admin', 'Cisc0123', cmd='show ip int brief')
+print(f'\nThere are {len(hosts)} available hosts in subnet {subnet}: \n{hosts}')
+print('Now try to get interface information of all hosts except gateway: ')
+pprint.pprint(qytang_get_if(*hosts))
