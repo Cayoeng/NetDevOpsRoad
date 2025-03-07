3885cf410926110cf0c6acbfbf5b4642ba072e06
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-07 11:47:32 +0800

    2025-3-7 homework.troubleshooting2

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-07/get_diff.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-07/get_diff.py
@@ -18,18 +18,15 @@ def qytang_check_diff(ip):
     before_md5 = ''
     while True:
         time.sleep(5)
-        running_config = qytang_get_config(ip)
-        trim_config = re.match(r'(^hostname[\s\S]*?end$)', running_config).groups()
-        print('*'*64)
-        print(trim_config)
-        print('*' * 64)
-        if trim_config:
-            current_md5 = hashlib.md5(trim_config[0].encode('utf-8')).hexdigest()
-            if current_md5 != before_md5:
-                before_md5 = current_md5
-                return print(f'\nConfig has been changed on {ip}!')
-            else:
-                return before_md5
+
+        trim_config = re.findall(r'(^hostname[\s\S]*?end$)', qytang_get_config(ip))
+        current_md5 = hashlib.md5(trim_config[0].encode('utf-8')).hexdigest()
+
+        if current_md5 != before_md5:
+            before_md5 = current_md5
+            return print(f'\nConfig has been changed on {ip}!')
+        else:
+            return before_md5
 
 
 if __name__ == '__main__':
