e5293af3a8560e5d1470d8bc51490fa3888ac49d
Cayoeng <158607841+Cayoeng@users.noreply.github.com>
2025-03-07 12:21:19 +0800

    2025-3-7 homework. change match pattern

--- NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-07/get_diff.py
+++ NetDevOpsRoad/PartOne_PythonBasic/HomeWork/2025-03-07/get_diff.py
@@ -18,11 +18,11 @@ def qytang_check_diff(ip):
     before_md5 = ''
     while True:
         time.sleep(5)
-        print(qytang_get_config(ip))
-        trim_config = re.findall('^hostname', qytang_get_config(ip), re.DOTALL)
-        print(trim_config)
+
+#        trim_config = re.findall('^hostname', qytang_get_config(ip), re.DOTALL)
+        trim_config = qytang_get_config(ip)
         current_md5 = hashlib.md5(trim_config[0].encode('utf-8')).hexdigest()
-        print(current_md5)
+
         if current_md5 != before_md5:
             before_md5 = current_md5
             return print(f'\nConfig has been changed on {ip}!')
